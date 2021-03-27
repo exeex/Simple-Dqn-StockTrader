0edda17954fa603f53ad119f68faea0a5d7dd75f
cswu <xray0h@gmail.com>
2021-03-27 15:17:41 +0800

    2 column, fix stock_num not reset bug

--- stock_env.py
+++ stock_env.py
@@ -13,10 +13,12 @@ class StockTradeAgent:
         self.action_space = ['buy', 'hodl', 'sell']  # 買1股, 不買不賣, 賣1股(或賣空1股)
         self.n_actions = len(self.action_space)
 
-        self.window_size = 10  # 根據過去多少天的資料做決策
-        column = 1  # 觀察的欄位數目
-        self.n_features = self.window_size * column  # input vector大小
-        self.dates, self.dataset = self._build_dataset("TSM")  # 台積電美股代號
+        self.dates, self.stock_price_data, self.train_data = self._build_dataset(
+            "TSM")  # 台積電美股代號
+
+        self.window_size = 30  # 根據過去多少天的資料做決策
+        column = self.train_data.shape[0]  # 觀察的欄位數目
+        self.n_features = self.window_size * column  # model input vector大小
 
         self.capital = 10000.0  # 本金(USD)
         self.stock_num = 0  # 持有股數 (若為負值 則為賣空股數)
@@ -25,18 +27,38 @@ class StockTradeAgent:
         self._select_idx = 0
 
     def _build_dataset(self, stock_name):
-        # Complete the dataset loader function
+
         dataset = data_reader.DataReader(stock_name, data_source="yahoo")
 
+        # build original dataset
         dates = [str(date) for date in dataset.index]
         close = dataset['Close'].to_numpy()  # 休市價格
+        stock_price_data = np.stack([close], axis=0)  # shape = [1, Date]
 
-        data = np.stack([close], axis=0)  # shape = [Column, Date]
+        # build transformed dataset (for model training) 
+        """
+        說明:
+        你可以在此處自行設計策略!!!
+        只要確保train_data 輸出shape為 [欄位書目, 資料筆數] 即可
 
-        return dates, data
+        習題: 增加周線/月線/季線的資料欄位
+        Hint: 對休市價格(Close) 做移動平均濾波器(Moving Average Filter)
+        """
+        close = dataset['Close'].to_numpy()  # 休市價格
+        volume = dataset['Volume'].to_numpy()  # 交易量
 
-    def get_window_features(self, idx):
-        return self.dataset[:, idx-self.window_size: idx].flatten()  # 注意idx會不會存取越界!
+        # global normalize
+        norm_close = close / close.max()
+        norm_volume = volume / volume.max()
+
+        train_data = np.stack([norm_close, norm_volume],
+                              axis=0)  # shape = [欄位書目, 資料筆數]
+
+        return dates, stock_price_data, train_data
+
+    def get_observations(self, idx):
+        # 注意idx會不會存取越界!
+        return self.train_data[:, idx-self.window_size: idx].flatten()
 
     def get_date(self):
         return str(self.dates[self._select_idx]).split()[0]
@@ -47,56 +69,50 @@ class StockTradeAgent:
         self._account_balance = self.capital
 
         # 隨機選擇某日開始操作股票
-        self._select_idx = random.randint(self.window_size, self.dataset.shape[1]-1)  # 注意idx會不會存取越界!
+        self.stock_num = 0
+        self._select_idx = random.randint(
+            self.window_size, self.stock_price_data.shape[1]-2)  # 注意idx會不會存取越界!
 
         print(f"以{self.get_date()}為初始日期開始模擬操盤..")
 
         # return observation
-        return self.get_window_features(self._select_idx)
+        return self.get_observations(self._select_idx)
 
     def step(self, action_id):
 
         # 處理action
-        stock_price = self.dataset[0, self._select_idx]
+        stock_price = self.stock_price_data[0, self._select_idx]
 
         if action_id == 0:  # buy
             self._account_balance -= stock_price
-            self.stock_num +=1
-        
+            self.stock_num += 1
         elif action_id == 1:  # hodl
             pass
-
         elif action_id == 2:  # sell
             self._account_balance += stock_price
-            self.stock_num -=1
+            self.stock_num -= 1
 
         # 前進一天
         self._select_idx += 1
 
-            
-
         # 計算新一天的 observation
-        s_ =self.get_window_features(self._select_idx)
-
+        s_ = self.get_observations(self._select_idx)
 
         # 計算Reward
+        account_value = self.stock_num * stock_price + self._account_balance  # 計算當前帳戶淨值
+        profit = account_value / self.capital - 1  # 當前利潤率
 
-        account_value = self.stock_num * stock_price + self._account_balance #計算當前帳戶淨值
-        profit = account_value / self.capital - 1 # 當前利潤率
-        
-        
-        print(f"{self.get_date()} : {self.action_space[int(action_id)]}, profit= {profit}")
+        print(
+            f"{self.get_date()} : {self.action_space[int(action_id)]}, profit= {profit}")
 
-        if profit <= -0.2: # 利潤小於-20%止損出場
+        if profit <= -0.2:  # 利潤小於-20%止損出場
             reward = -1.0
-            done = True 
+            done = True
         else:
             reward = profit
             done = False
-        
 
-        if self._select_idx+1 >= self.dataset.shape[1]:
+        if self._select_idx+1 >= self.stock_price_data.shape[1]:  # 若日期超出範圍停止模擬
             done = True
 
         return s_, reward, done
-
